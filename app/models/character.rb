7b8a993e02d38f6d04185608af969e151b5823cf
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-12 21:39:53 +0000

    chracter belongs to a movie

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,13 +10,26 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
-  def movie
-    key = self.movie_id
+  #belongs_to(:name_that_we_want, class_name: "", foreign_key: "" )
+  
+  #Character belongs to a movie 
+  belongs_to( :movie, class_name: "Movie", foreign_key: "movie_id" )
 
-    matching_set = Movie.where({ :id => key })
+#  def movie
+#    key = self.movie_id
+
+#    matching_set = Movie.where({ :id => key })
+
+#    the_one = matching_set.at(0)
+
+#    return the_one
+#  end
+
+  def actor
+    key = self.actor_id
+
+    matching_set = Actor.where({:id => key})
 
     the_one = matching_set.at(0)
-
-    return the_one
   end
 end
